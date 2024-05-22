d3b8584205337dd176fa42af6a51fe79ef72da5d
Brendan Dempsey <bjd1412@gmail.com>
2024-05-22 02:08:41 -0400

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", encoding='utf-8', mode = "w") as file:
+        file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", encoding='utf-8', mode= "a") as file:
+        file.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f"{file_name}.txt", encoding='utf-8', mode = "r") as file:
+        return file.read()
+
     pass
